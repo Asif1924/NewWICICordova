7d54de851c1edb82d979e0a00753a5b39cb902fa
Asif Alli <alli.asif@gmail.com>
2022-06-27 18:52:16 -0400

    added vibrate and device plugins

--- www/js/WICIPlugin.js
+++ www/js/WICIPlugin.js
@@ -1,8 +1,19 @@
 document.getElementById("callCordova").addEventListener("click", callCordova);
+document.getElementById("vibrate").addEventListener("click", vibrate);
+document.getElementById("deviceInfo").addEventListener("click", getDeviceInfo);
 
 function callCordova() {
     cordova.exec(success,null,"WICIPlugin","coolMethod",["Hello, world from plugin"]);
 } 
+
+function vibrate() {
+    navigator.vibrate([10000]);
+} 
+
+function getDeviceInfo() {
+    alert("device.serial=" + device.serial + "\ndevice.model=" + device.model);
+} 
+
 
 function success(result){
     alert(result);        
